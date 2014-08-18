ccb91a62c39a97f18add3d29175e3c713708eb24
SuperSickler <steven.sickler@yahoo.com>
2014-08-18 14:48:24 -0500

    Initial Commit

---
+++ GitPractice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitPractice
+//
+//  Created by Steven Sickler on 8/18/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
